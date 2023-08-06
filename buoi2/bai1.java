bd5892d4a063fdd375962c0a4382faca16f5626a
duahau2130 <auditionkaka@gmail.com>
2023-08-06 15:06:47 +0700

    init project

--- buoi2/bai1.java
+++ buoi2/bai1.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class bai1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("nhap n = ");
+        System.out.print("nhap n = ");
         int n = sc.nextInt();
         for(int i = 1 ; i <= n ;i++){
             for(int j = 1 ; j <= n ;j++){
                 if(i == 1 || i == n || j == 1 || j == n)
-                    System.out.println("*");
+                    System.out.print("*");
                 else   
-                    System.out.println(" ");
+                    System.out.print(" ");
             }
-            System.out.println("\n");
+            System.out.print("\n");
         }
     }
     
